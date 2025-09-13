5c3a12d52f5e24f9b4fc7d549335db13a6665c8d
Portela03 <joao.portela03@gmail.com>
2025-09-13 16:33:18 -0300

    feat: adicionar suporte a variáveis de ambiente e configurar URL base da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ import type {
   ValidateTokenResponse 
 } from '../types';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: BASE_URL,
